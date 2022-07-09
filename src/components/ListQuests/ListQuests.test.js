d33509f63718ed8e88c6e4682a7324ee224e0e23
Gabriel Alves <gabrielsalves2k18@gmail.com>
2022-07-09 12:32:16 -0300

    Adiciona testes

--- src/components/ListQuests/ListQuests.test.js
+++ src/components/ListQuests/ListQuests.test.js
@@ -6,7 +6,6 @@ describe('Renderiza a lista de quests de um projeto', () => {
   it('e deve conter o texto Projeto e Status', () => {
     render(<ListQuests/>, {wrapper: MemoryRouter })
 
-    expect(screen.getByTestId('formSelect')).toBeInTheDocument();
     expect(screen.getByRole("link", { name: /Criar/i})).toBeInTheDocument();
   });
 })
