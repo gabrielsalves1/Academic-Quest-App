97e2977588fd55034ddee3c7482bc929fcdb4644
hoffmannpaola <hoffmannpaola@gmail.com>
2022-08-17 14:17:51 -0300

    code refactor

--- src/pages/Admin/Dashboard/index.jsx
+++ src/pages/Admin/Dashboard/index.jsx
@@ -4,15 +4,9 @@ import { Chart } from "react-google-charts";
 import _ from "lodash"
 import { getData } from "../../../service/requests";
 import { RiContactsBookLine } from "react-icons/ri";
-
+import Container from "../../../components/Container";
 
 export default function Dashboard() {
-  const [ loading, setLoading ] = useState();
-  const [ chartData, setChartData] = useState([])
-  const [ dataFinal, setDataFinal] = useState([])
-  const [ allOpenQuests, setAllOpenQuests] = useState([])
-  // const [ options, setOptions] = useState({})
-  
   const loadData = (data) => {
     const values = _.groupBy(data, (value) => value.status)
     
@@ -26,88 +20,96 @@ export default function Dashboard() {
     return [ ["status", "qtd grupos"], ...result,]
   }
 
+  const result = 
+  {
+    "result" : { 
+    "projeto" : "TCC1",
+    "statusProjeto" : "EM_ANDAMENTO",
+    "tarefas" : 
+      [
+        {
+          "nomeTarefa" : "Introdução",
+          "dataEntrega" : "xx/xx/xxxx",
+          "status":  "aberta",
+          "tarefasGrupo" : [
+            {
+              "nomeGrupo" : "Fumacas",
+              "status" : "PENDENTE"
+            },
+            {
+              "nomeGrupo" : "Boston Dynamics",
+              "status" : "ENTREGUE"
+            },
+            {
+              "nomeGrupo" : "Paola",
+              "status" : "ENTREGUE"
+            },
+            {
+              "nomeGrupo" : "Paola",
+              "status" : "ENTREGUE"
+            },
+            {
+              "nomeGrupo" : "Os Nadas",
+              "status" : "CORRIGIDA"
+            }
+          ]
+        },
+        {
+          "nomeTarefa" : "Cronograma",
+          "dataEntrega" : "xx/xx/xxxx",
+          "status":  "aberta",
+          "tarefasGrupo" : [
+              {
+                  "nomeGrupo" : "Fumacas",
+                  "status" : "CORRIGIDA"
+              },
+              {
+                  "nomeGrupo" : "Boston Dynamics",
+                  "status" : "Entregue"
+              }
+                ]
+              }
+      ]
+  }
+  }
+
   useEffect (() => { 
     // pegar da api
     // getData(`/materias`, setData, setLoading);
  
-
-    const data2 = 
-    {
-      "result" : { 
-      "projeto" : "TCC1",
-      "statusProjeto" : "EM_ANDAMENTO",
-      "tarefas" : 
-        [
-          {
-            "nomeTarefa" : "Introdução",
-            "dataEntrega" : "xx/xx/xxxx",
-            "status":  "aberta",
-            "tarefasGrupo" : [
-              {
-                "nomeGrupo" : "Fumacas",
-                "status" : "PENDENTE"
-              },
-              {
-                "nomeGrupo" : "Boston Dynamics",
-                "status" : "ENTREGUE"
-              },
-              {
-                "nomeGrupo" : "Paola",
-                "status" : "ENTREGUE"
-              },
-              {
-                "nomeGrupo" : "Paola",
-                "status" : "ENTREGUE"
-              },
-              {
-                "nomeGrupo" : "Os Nadas",
-                "status" : "CORRIGIDA"
-              }
-            ]
-          },
-          {
-            "nomeTarefa" : "Cronograma",
-            "dataEntrega" : "xx/xx/xxxx",
-            "status":  "aberta",
-            "tarefasGrupo" : [
-                {
-                    "nomeGrupo" : "Fumacas",
-                    "status" : "CORRIGIDA"
-                },
-                {
-                    "nomeGrupo" : "Boston Dynamics",
-                    "status" : "Entregue"
-                }
-                  ]
-                }
-        ]
-    }
-    }
-
-    data2["result"]["tarefas"].forEach((tarefa)  =>
-      {if (tarefa.status == "aberta") {
-        setDataFinal(loadData(tarefa.tarefasGrupo))      
-      };
-    })
+    // data2["result"]["tarefas"].forEach((tarefa)  =>
+    //   {if (tarefa.status == "aberta") {
+    //     setDataFinal(loadData(tarefa.tarefasGrupo))      
+    //   };
+    // })
 
   }, [])
   
-  const options = {
-    title: "Quest 3 - Requisitos do projeto",
-    colors: ["#845EC2", "#D65DB1", "#5D8DD6"]
-  };
+
+  
  
   
   return (
-    <>    
-      <Chart
-        chartType="PieChart"
-        options={options}
-        data={dataFinal}
-        width={"1000px"}
-        height={"1000px"}
-        
-      />      
+    <>   
+      <div classStyle="containerDisplayFlex">
+        {result["result"]["tarefas"].map((tarefa)  => {
+          {if (tarefa.status == "aberta") {
+            const options = {
+              title: tarefa.nomeTarefa,
+              colors: ["#845EC2", "#D65DB1", "#5D8DD6"]
+            };
+            return (
+              <Chart
+                chartType="PieChart"
+                options={options}
+                data={loadData(tarefa.tarefasGrupo)}
+                width={"500px"}
+                height={"500px"}
+              />  
+            )
+          }}
+        })}
+      </div> 
     </>
   )
 }
