446d6f65b44180bc6523e751a8a2a70d31c27421
hoffmannpaola <hoffmannpaola@gmail.com>
2022-08-19 15:25:00 -0300

    add new charts

---
+++ src/components/BallStatusDanger/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import style from "./BallStatusDanger.module.scss";
+
+export default function BallStatus() {
+  return (
+    <div className={style.padding10}>
+      <div className={style.configStatusDanger }>
+      </div>
+    </div>
+    );
+}
