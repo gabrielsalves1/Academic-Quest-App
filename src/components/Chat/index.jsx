613cd8019eab74bb0eecf9650ff2d65595ef1655
hoffmannpaola <hoffmannpaola@gmail.com>
2022-08-31 21:23:49 -0300

    userId validation

--- src/components/Chat/index.jsx
+++ src/components/Chat/index.jsx
@@ -24,45 +24,54 @@ export default function Chat(props) {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const [ messages, setMessages ] = useState(props.messages.chats);
 
+  const idUser = sessionStorage.getItem('idUser')
 
   const onSubmit = message => {
     const msg = message
     const data = {
       "mensagem": msg.mensagem,
       "tarefaGrupoId": props.idTaskGroup,
-      "userId": sessionStorage.setItem('idUser', res.data.id)
+      "userId": idUser
     }
 
     postMessageChat(data, `/project/${props.idProject}/view-task/${props.idQuest}/task-group/${props.idTaskGroup}`)
   }
 
+
+
   return (
-    <div className={style.containerChat}>
-        <div className={style.chatBox} >
-          <div className={style.chatBoxScroll}>
-            {messages?.map((msg) => {
-                console.log(msg.mensagem)
-              return (
-                <div className={style.marginTopChat}>
-                  <MessageBalloonSent message={msg.mensagem} nickName={msg.primeiroNome + ' ' + msg.segundoNome}/>
-                </div>
-              )
-            })}
-          </div>
-         
-          <Form onSubmit = { handleSubmit(onSubmit) }>
-            <div className={style.displayFlexRow}>
-              <>
-                <Form.Group>
-                  <Form.Control as="textarea" name="mensagem"  {...register("mensagem", { required: true })} className={style.chatInputArea}/>
-                </Form.Group>
-              </>
-              <div className={style.boxBtnSend}>
-                <button type="submit" className={style.button}><RiSendPlane2Fill className={style.icon}/></button>
+    <>
+        <div className={style.containerChat}>
+            <div className={style.chatBox} >
+              <div id="scroll" className={style.chatBoxScroll}>
+                {(messages.length > 0 ) ? (
+                  messages.map((msg) => {
+                    return (
+                      <div key={msg.id} className={style.marginTopChat}>
+                        <MessageBalloonSent message={msg.mensagem} nickName={msg.primeiroNome + ' ' + msg.segundoNome}/>
+                      </div>
+                    )
+                  })
+                ) : 
+                (<div className={style.boxWithoutMsg}>
+                  <span className={style.textWithoutMsg}> Não há mensagens!</span>
+                </div>)}
               </div>
+            
+              <Form onSubmit = { handleSubmit(onSubmit) }>
+                <div className={style.displayFlexRow}>
+                  <>
+                    <Form.Group>
+                      <Form.Control as="textarea" name="mensagem"  {...register("mensagem", { required: true })} className={style.chatInputArea}/>
+                    </Form.Group>
+                  </>
+                  <div className={style.boxBtnSend}>
+                    <button type="submit" className={style.button}><RiSendPlane2Fill className={style.icon}/></button>
+                  </div>
+                </div>
+              </Form>
             </div>
-          </Form>
         </div>
-      </div>
+    </>
   );
 }
