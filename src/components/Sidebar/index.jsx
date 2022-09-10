cc4d78d211503e8aee8d4f0034d284c070231067
hoffmannpaola <hoffmannpaola@gmail.com>
2022-09-10 14:16:17 -0300

    layout ok

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,27 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import { HiChevronDoubleRight } from "react-icons/hi";
+import { Image } from "react-bootstrap";
+import { HiChevronDoubleRight, HiChevronDoubleLeft } from "react-icons/hi";
 import { BsBarChartFill } from "react-icons/bs";
 import { FaFolder } from "react-icons/fa";
 import { HiUserGroup } from "react-icons/hi";
 import { AiFillHome } from "react-icons/ai";
-import { RiLogoutCircleRLine } from "react-icons/ri";
+import { MdLogout } from "react-icons/md";
 import style from './Sidebar.module.scss';
 
-
+import { Context } from "../../Context/AuthContext";
+import iconLogo from '../../assets/img/icon-academic-quest.svg';
 
 export default function Sidebar() {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const { handleLogout } = useContext(Context);
+  const [ userFirstName, setUserFirstName] = useState(sessionStorage.getItem('userFirstName'));
+  const [ userRole, setUserRole ] = useState(sessionStorage.getItem('role'));
+
 
   return (
     <>
       <div className={style.sidebarClosed}>
+      <div className={style.boxHeader}>
+        <div>
+          <Image fluid src={iconLogo} className={style.novoLogoStyle}/>
+        </div>
+
         <div className={style.boxIcon}>
           <HiChevronDoubleRight onClick={handleShow} className={style.iconStyle} data-testid="sidebar"/>
         </div>
+      </div>
+
+        
 
         <div className={style.containerLittleBoxMenu}>
           <a href='/' className={style.littleBoxMenu}>
@@ -39,38 +53,56 @@ export default function Sidebar() {
         </div>
 
         
-        <a  href='/logout' className={style.sair}>
+        <span onClick={handleLogout} className={style.sair}>
           Sair
-          <RiLogoutCircleRLine className={style.iconSair}/>
-        </a>
+          <MdLogout className={style.iconSair}/>
+        </span>
       </div>
 
       <Offcanvas show={show} onHide={handleClose} className={style.sidebarOpen}>
-        <Offcanvas.Header className={style.iconStyle} closeButton>
-        </Offcanvas.Header>
-        <div className={style.containerLittleBoxMenu}>
-          <a href='/' className={style.boxMenu}>
-            <AiFillHome className={style.iconMenuHome} />
-            <span className={style.nameMenuHome}>Home</span>
-          </a>
-          <a href='/dashboard' className={style.boxMenu}>
-            <BsBarChartFill className={style.iconMenuGrid} />
-            <span className={style.nameMenuGrid}>Dashboard</span>
-          </a>
-          <a  href='/projects' className={style.boxMenu}>
-            <FaFolder className={style.iconMenuFolder} />
-            <span className={style.nameMenuFolder}>Projetos</span>
-          </a>
-          <a  href='/groups' className={style.boxMenu}>
-            <HiUserGroup className={style.iconMenuGroup} />
-            <span className={style.nameMenuGroup}>Grupos</span>
-          </a>
+        <div className={style.boxHeader}>
+          <div>
+            <Image fluid src={iconLogo} className={style.novoLogoStyle}/>
+          </div>
+          <span className={style.academicQuest}>Academic <br/> Quest</span>
+          <div>
+            <HiChevronDoubleLeft onClick={handleClose} className={style.iconStyle} data-testid="sidebar"/>
+          </div>
+        </div>
+        
+        <div className={style.divCenter}>
+          <div className={style.boxName}>
+            <p className={style.userFirstName}>{userFirstName}</p>
+            <p className={style.userRole}>
+              {userRole == "PROFESSOR" ? "Professor" : "Aluno"}
+            </p>
+          </div>
+        
+          <div className={style.containerLittleBoxMenu}>
+            <a href='/' className={style.boxMenu}>
+              <AiFillHome className={style.iconMenuHome} />
+              <span className={style.nameMenuHome}>Home</span>
+            </a>
+            <a href='/dashboard' className={style.boxMenu}>
+              <BsBarChartFill className={style.iconMenuGrid} />
+              <span className={style.nameMenuGrid}>Dashboard</span>
+            </a>
+            <a  href='/projects' className={style.boxMenu}>
+              <FaFolder className={style.iconMenuFolder} />
+              <span className={style.nameMenuFolder}>Projetos</span>
+            </a>
+            <a  href='/groups' className={style.boxMenu}>
+              <HiUserGroup className={style.iconMenuGroup} />
+              <span className={style.nameMenuGroup}>Grupos</span>
+            </a>
+          </div>
         </div>
 
-        <a  href='/logout' className={style.sair}>
+
+        <span onClick={handleLogout} className={style.sair}>
           Sair
-          <RiLogoutCircleRLine className={style.iconSair}/>
-        </a>
+          <MdLogout className={style.iconSair}/>
+        </span>
       </Offcanvas>
     </>
   );
