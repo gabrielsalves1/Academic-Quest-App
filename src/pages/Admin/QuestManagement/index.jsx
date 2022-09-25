8d1b0dee19e11df0484a1cc5636873692540db46
Hoffmann Paola <hoffmannpaola@gmail.com>
2022-09-25 10:59:42 -0300

    gerenciamento de quest

--- src/pages/Admin/QuestManagement/index.jsx
+++ src/pages/Admin/QuestManagement/index.jsx
@@ -3,18 +3,21 @@ import { useParams } from "react-router-dom";
 import style from "./QuestManagement.module.scss";
 
 import ListQuests from "../../../components/ListQuests";
-import Container from "../../../components/Container";
+import NewContainer from "../../../components/NewContainer";
+import ComeBackButtonIcon from "../../../components/ComeBackButtonIcon";
 
 export default function QuestManagement(props) {
   const { idProject } = useParams();
 
   return (
-    <Container classStyle="containerAlignCenter">
-      <h1 className={style.title}>
-        Gerenciamento de Quest
-      </h1>
+    <NewContainer>
+       <ComeBackButtonIcon url={`/projects`}> </ComeBackButtonIcon>
+        <h1 className={style.title}>
+          Gerenciamento de Quest
+        </h1>
+        
 
       <ListQuests idProject={idProject}></ListQuests>
-    </Container>
+    </NewContainer>
   );
 }
