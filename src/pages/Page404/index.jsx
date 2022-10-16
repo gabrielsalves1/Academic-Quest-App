56e6420fa5b5c2c83a3239c65a7c70380baaf571
Gabriel <gabrielsalves2k18@gmail.com>
2022-10-16 12:50:34 -0300

    Validate task pending student

--- src/pages/Page404/index.jsx
+++ src/pages/Page404/index.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import style from "./Page404.module.scss";
 import { IoAlertCircleSharp } from "react-icons/io5";
 
-import Container from "../../components/Container";
 import LinkButton from "../../components/LinkButton";
+import NewContainer from "../../components/NewContainer";
 
 export default function Page404() {
   return (
-    <Container classStyle="containerJustifyCenter">
+    <NewContainer>
       <h2 className={style.msg}>Página não encontrada ou você não possui permissão para visualizá-la <IoAlertCircleSharp className={style.icon}/></h2>
       { !sessionStorage.getItem('token') &&
         <h2 className={style.msg}>Caso não tenha iniciado sua sessão, <LinkButton to="/login">Clique aqui</LinkButton></h2>
       }
-    </Container>
+    </NewContainer>
   );
 }
